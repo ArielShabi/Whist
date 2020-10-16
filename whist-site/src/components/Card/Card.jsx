c58231fd7dfc2e8cb1a31e4d94d93bd9576684d7
ArielShabi <4ariel6@gmail.com>
2020-10-16 09:12:10 +0300

    added image to card

--- whist-site/src/components/Card/Card.jsx
+++ whist-site/src/components/Card/Card.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { CardInfo } from '../../common/CardInfo';
 import './card.css';
+import { cardTranslator } from '../../utils';
 
-const Card = (props) => (
-    <div className="card">
-        TEST
-    </div>
-);
+const pathToImageFolder = '/Cards';
+
+const Card = ({ cardInfo }) => {
+    const imageSrc = useMemo(() => {
+        const cardString = cardTranslator.cardToString(cardInfo);
+        return `${pathToImageFolder}/${cardString}.jpg`;
+    }, [cardInfo])
+
+    return (
+        <div className="card">
+            <img src={imageSrc} />
+        </div>)
+};
+
+Card.propTypes = {
+    cardInfo: PropTypes.instanceOf(CardInfo)
+};
 
 export default Card;
