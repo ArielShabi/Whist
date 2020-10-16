bb28c34fc8b17442af40bca0178690920c310c74
ArielShabi <4ariel6@gmail.com>
2020-10-16 09:12:34 +0300

    tests that everything works

---
+++ whist-site/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserHand from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <UserHand />
+    </div>
+  );
+}
+
+export default App;
--- whist-site/src/components/UserHand/UserHand.jsx
+++ whist-site/src/components/UserHand/UserHand.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './user-hand.css';
+import Card from '../Card/Card';
+import { CardInfo } from '../../common/CardInfo';
 
 const UserHand = (props) => (
     <div className="user-hand">
-        TEST
+        <Card cardInfo={new CardInfo(5,'C')}/>
     </div>
 );
 
