2c0a087b5135fa15fe3d0debd1af10ba12194e92
ArielShabi <4ariel6@gmail.com>
2020-12-09 20:02:15 +0200

    ts user container

--- whist-server/src/webSocketServer/userContainer/userContainer.ts
+++ whist-server/src/webSocketServer/userContainer/userContainer.ts
@@ -5,31 +5,39 @@ import { User } from './types';
 const userContainer = () => {
     let users: User[] = [];
 
-    const addUser = connection => {
-        const id = uniqid();
+    const addUser = (connection: WebSocket): string => {
+        const id: string = uniqid();
         const name = getRandomName();
+
         users.push({
             id,
             connection,
             name
-        })
+        });
+
         return id;
     };
 
-    const removeUser = id => {
+    const removeUser = (id: string): boolean => {
+        const numberOfUsers = users.length;
         users = users.filter(user => user.id !== id);
+        return numberOfUsers !== users.length;
     }
 
-    const editUser = (id, newUserData) => {
+    const editUser = (id: string, newUserData: Partial<User>): User => {
         const userToEditIndex = users.findIndex(user => user.id === id);
-        const userToEdit = users[userToEditIndex];        
+        const userToEdit = users[userToEditIndex];
         users[userToEditIndex] = { ...userToEdit, ...newUserData };
         return users[userToEditIndex];
     }
 
-    const getUser = (id) => users.find(user => user.id === id);
+    const getUser = (id: string): User => {
+        return users.find(user => user.id === id)
+    };
 
-    const getAllOpenUsers = (idToExclude) => users.filter(user => user.id != idToExclude && user.connection.readyState == WebSocket.OPEN);
+    const getAllOpenUsers = (idToExclude: string): User[] => {
+        return users.filter(user => user.id != idToExclude && user.connection.readyState == WebSocket.OPEN);
+    }
 
     return {
         addUser,
