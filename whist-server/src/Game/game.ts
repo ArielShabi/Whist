c89f26bf9a19832a2a60da983279e96970b59dbb
ArielShabi <4ariel6@gmail.com>
2020-12-09 23:19:01 +0200

    added getRoundWinner and resetBoard functinos

--- whist-server/src/Game/game.ts
+++ whist-server/src/Game/game.ts
@@ -1,8 +1,12 @@
 
-import { PlayerCard, PlayingUser } from './types';
+import { CardSuits, PlayerCard, PlayingUser } from './types';
 
-const game = (users: PlayingUser[]): void => {
+const game = (users: PlayingUser[], strongSuit: CardSuits): any => {
     const board: PlayerCard[] = [];
+
+    const resetBoard = (): void => {
+        board.splice(0, board.length);
+    }
 
     const playCard = (card: PlayerCard): string => {
         const isSpaceTaken = board.some(playerCard => playerCard.id === card.id);
@@ -18,7 +22,32 @@ const game = (users: PlayingUser[]): void => {
         return nextUser.id;
     };
 
+    const isBoardFull = (): boolean => {
+        return board.length === users.length;
+    };
 
+    const getRoundWinner = (): string => {
+        if (!isBoardFull()) {
+            throw new Error('Cannot decide on winner if board is not full');
+        }
+
+        const winner = board.reduce((currentWinner, playerCard) => {
+            if (currentWinner.suit === playerCard.suit) {
+                return playerCard.number > currentWinner.number ? playerCard : currentWinner;
+            }
+
+            return playerCard.suit === strongSuit ? playerCard : currentWinner;
+        }, board[0]);
+
+        return winner.id;
+    };
+
+    return {
+        resetBoard,
+        playCard,
+        isBoardFull,
+        getRoundWinner
+    };
 };
 
 export default game
