6f195b084cb81af7a317e1a5b4fd2dddf5072627
ArielShabi <4ariel6@gmail.com>
2020-12-18 19:59:21 +0200

    moved all board related function to board.ts

--- whist-server/src/Game/board.ts
+++ whist-server/src/Game/board.ts
@@ -1,16 +1,52 @@
-const board = () => {
-    const cards = {};
+import { CardSuits, PlayerCard, PlayingUser } from './types';
 
-    const playCard = (playerId, card) => {
-        if (!cards[playerId]) {
-            throw new Error('Card place is not empty');
+const createBoard = (users: PlayingUser[], strongSuit: CardSuits) => {
+    const cards: PlayerCard[] = [];
+
+    const playCard = (card: PlayerCard): string => {
+        const isSpaceTaken = cards.some(playerCard => playerCard.id === card.id);
+
+        if (isSpaceTaken) {
+            throw new Error(`Player all ready played card ${card}`);
         }
 
-        cards[playerId] = card;
-    }
+        cards.push(card);
+        const userSeatNumber = users.findIndex(user => user.id == card.id);
+        const nextUser = users[(userSeatNumber + 1) % users.length];
 
-    const clearBoard = () =>{
-        
-    }
+        return nextUser.id;
+    };
 
-}
+    const resetBoard = (): void => {
+        cards.splice(0, cards.length);
+    };
+
+    const isBoardFull = (): boolean => {
+        return cards.length === users.length;
+    };
+
+    const getRoundWinner = (): string => {
+        if (!isBoardFull()) {
+            throw new Error('Cannot decide on winner if board is not full');
+        }
+
+        const winner = cards.reduce((currentWinner, playerCard) => {
+            if (currentWinner.suit === playerCard.suit) {
+                return playerCard.number > currentWinner.number ? playerCard : currentWinner;
+            }
+
+            return playerCard.suit === strongSuit ? playerCard : currentWinner;
+        }, cards[0]);
+
+        return winner.id;
+    };
+
+    return {
+        playCard,
+        resetBoard,
+        isBoardFull,
+        getRoundWinner
+    };
+};
+
+export default createBoard;
--- whist-server/src/Game/game.ts
+++ whist-server/src/Game/game.ts
@@ -1,52 +1,13 @@
 
+import createBoard from './board';
 import { CardSuits, PlayerCard, PlayingUser } from './types';
 
 const game = (users: PlayingUser[], strongSuit: CardSuits): any => {
-    const board: PlayerCard[] = [];
+    const board = createBoard(users, strongSuit);
 
-    const resetBoard = (): void => {
-        board.splice(0, board.length);
-    }
-
-    const playCard = (card: PlayerCard): string => {
-        const isSpaceTaken = board.some(playerCard => playerCard.id === card.id);
-
-        if (isSpaceTaken) {
-            throw new Error(`Player all ready played card ${card}`);
-        }
-
-        board.push(card);
-        const userSeatNumber = users.findIndex(user => user.id == card.id);
-        const nextUser = users[(userSeatNumber + 1) % users.length];
-
-        return nextUser.id;
-    };
-
-    const isBoardFull = (): boolean => {
-        return board.length === users.length;
-    };
-
-    const getRoundWinner = (): string => {
-        if (!isBoardFull()) {
-            throw new Error('Cannot decide on winner if board is not full');
-        }
-
-        const winner = board.reduce((currentWinner, playerCard) => {
-            if (currentWinner.suit === playerCard.suit) {
-                return playerCard.number > currentWinner.number ? playerCard : currentWinner;
-            }
-
-            return playerCard.suit === strongSuit ? playerCard : currentWinner;
-        }, board[0]);
-
-        return winner.id;
-    };
 
     return {
-        resetBoard,
-        playCard,
-        isBoardFull,
-        getRoundWinner
+
     };
 };
 
