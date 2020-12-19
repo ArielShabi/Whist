062c3d20e2601b36097fe2dae088a1acc16735db
ArielShabi <4ariel6@gmail.com>
2020-12-19 15:16:37 +0200

    player played a card logic

--- whist-server/src/Game/game.ts
+++ whist-server/src/Game/game.ts
@@ -1,10 +1,26 @@
 
 import createBoard from './board';
 import { CardSuits, PlayerCard, PlayingUser } from './types';
+import playerCommunicator from './gamePlayerCommunicator';
 
 const game = (users: PlayingUser[], strongSuit: CardSuits): any => {
     const board = createBoard(users, strongSuit);
 
+    const playerPlayed = (card: PlayerCard) => {
+        board.playCard(card);
+
+        playerCommunicator.cardPlayed(users, card);
+
+        if (board.isBoardFull) {
+            const winnerUserId = board.getRoundWinner();
+            //winning round logic            
+        }
+
+        const userSeatNumber = users.findIndex(user => user.id == card.id);
+        const nextUser = users[(userSeatNumber + 1) % users.length];
+
+        playerCommunicator.requestCard(nextUser);
+    };    
 
     return {
 
