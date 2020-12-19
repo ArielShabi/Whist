cfeeb2e59d26aa48d2c1b915b5ae8b9574af9e60
ArielShabi <4ariel6@gmail.com>
2020-12-19 15:13:03 +0200

    added some message type for game

--- whist-server/src/webSocketServer/messages/messageTypes.ts
+++ whist-server/src/webSocketServer/messages/messageTypes.ts
@@ -1,16 +1,11 @@
-const editUserInfoMessageType = 'EDIT_USER_INFO';
-const getRandomNameMessageType = 'GET_RANDOM_NAME';
+export const editUserInfoMessageType = 'EDIT_USER_INFO';
+export const getRandomNameMessageType = 'GET_RANDOM_NAME';
 
-const usersJoinedMessageType = 'USERS_JOINED';
-const userRemovedMessageType = 'USER_REMOVED';
-const userEditedMessageType = 'USER_EDITED';
-const otherUserEditedMessageTyped = 'OTHER_USER_EDITED';
+export const usersJoinedMessageType = 'USERS_JOINED';
+export const userRemovedMessageType = 'USER_REMOVED';
+export const userEditedMessageType = 'USER_EDITED';
+export const otherUserEditedMessageTyped = 'OTHER_USER_EDITED';
 
-export {
-    editUserInfoMessageType,
-    getRandomNameMessageType,
-    usersJoinedMessageType,
-    userRemovedMessageType,
-    userEditedMessageType,
-    otherUserEditedMessageTyped
-};
+export const requestCardType = 'GAME_REQUEST_CARD';
+export const cardPlayedType = 'GAME_CARD_PLAYED';
+export const roundWonType = 'GAME_ROUND_WON';
