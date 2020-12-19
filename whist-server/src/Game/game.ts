e96d06a143e08891bc9ce17192a2934c4cb31f23
ArielShabi <4ariel6@gmail.com>
2020-12-19 16:49:12 +0200

    extracted complete round function

--- whist-server/src/Game/game.ts
+++ whist-server/src/Game/game.ts
@@ -5,22 +5,35 @@ import playerCommunicator from './gamePlayerCommunicator';
 
 const game = (users: PlayingUser[], strongSuit: CardSuits): any => {
     const board = createBoard(users, strongSuit);
+    const playersPoints = users.reduce((accumulator, user) => {
+        return accumulator[user.id] = 0;
+    }, {});
 
-    const playerPlayed = (card: PlayerCard) => {
+    const playerPlayed = (card: PlayerCard): void => {
         board.playCard(card);
 
         playerCommunicator.cardPlayed(users, card);
 
         if (board.isBoardFull) {
-            const winnerUserId = board.getRoundWinner();
-            //winning round logic            
+            completeRound();
+            return;
         }
 
         const userSeatNumber = users.findIndex(user => user.id == card.id);
         const nextUser = users[(userSeatNumber + 1) % users.length];
 
         playerCommunicator.requestCard(nextUser);
-    };    
+    };
+
+    const completeRound = (): void => {
+        const winnerUserInfo = board.getRoundWinner();
+        const boardState = board.getBoardState();
+        playerCommunicator.roundWon(users, winnerUserInfo, boardState);
+        playersPoints[winnerUserInfo.id]++;
+        board.resetBoard();
+        const winnerUser = users.find(user => user.id === winnerUserInfo.id);
+        playerCommunicator.requestCard(winnerUser);
+    }
 
     return {
 
