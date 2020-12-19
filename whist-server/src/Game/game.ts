b729cc4173815853a00f720f81f04dd9ae11cf0b
ArielShabi <4ariel6@gmail.com>
2020-12-19 16:59:02 +0200

    renamed user to player and removed played cards from player hand

--- whist-server/src/Game/game.ts
+++ whist-server/src/Game/game.ts
@@ -3,36 +3,44 @@ import createBoard from './board';
 import { CardSuits, PlayerCard, PlayingUser } from './types';
 import playerCommunicator from './gamePlayerCommunicator';
 
-const game = (users: PlayingUser[], strongSuit: CardSuits): any => {
-    const board = createBoard(users, strongSuit);
-    const playersPoints = users.reduce((accumulator, user) => {
-        return accumulator[user.id] = 0;
+const game = (players: PlayingUser[], strongSuit: CardSuits): any => {
+    const board = createBoard(players, strongSuit);
+    const playersPoints = players.reduce((accumulator, player) => {
+        return accumulator[player.id] = 0;
     }, {});
 
     const playerPlayed = (card: PlayerCard): void => {
         board.playCard(card);
 
-        playerCommunicator.cardPlayed(users, card);
+        const currentPlayer = players.find(player => player.id === card.id);
+        currentPlayer.cards = currentPlayer.cards.filter(c => c.number !== card.number && c.suit !== card.suit);
+
+        playerCommunicator.cardPlayed(players, card);
 
         if (board.isBoardFull) {
             completeRound();
+
+            if (currentPlayer.cards.length === 0) {
+                //FINISH
+            }
+            
             return;
         }
 
-        const userSeatNumber = users.findIndex(user => user.id == card.id);
-        const nextUser = users[(userSeatNumber + 1) % users.length];
+        const playerSeatNumber = players.findIndex(player => player.id == card.id);
+        const nextUser = players[(playerSeatNumber + 1) % players.length];
 
         playerCommunicator.requestCard(nextUser);
     };
 
     const completeRound = (): void => {
-        const winnerUserInfo = board.getRoundWinner();
+        const winnerInfo = board.getRoundWinner();
         const boardState = board.getBoardState();
-        playerCommunicator.roundWon(users, winnerUserInfo, boardState);
-        playersPoints[winnerUserInfo.id]++;
+        playerCommunicator.roundWon(players, winnerInfo, boardState);
+        playersPoints[winnerInfo.id]++;
         board.resetBoard();
-        const winnerUser = users.find(user => user.id === winnerUserInfo.id);
-        playerCommunicator.requestCard(winnerUser);
+        const winner = players.find(player => player.id === winnerInfo.id);
+        playerCommunicator.requestCard(winner);
     }
 
     return {
