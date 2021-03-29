c280deeab4ec7988e9c6c81484df114fbf3f9fd3
ArielShabi <4ariel6@gmail.com>
2021-03-29 14:02:41 +0300

    added user Container interface

--- whist-server/src/webSocketServer/userContainer/types.ts
+++ whist-server/src/webSocketServer/userContainer/types.ts
@@ -1,5 +1,13 @@
-import { UserInfo } from "../../types";
+import { UserInfo } from '../../types';
 
-export type User = UserInfo & {        
+export type User = UserInfo & {
     connection: WebSocketClient;
-}
+};
+
+export interface UserContainer {
+    addUser: (user: WebSocket) => string,
+    removeUser: (id: string) => boolean,
+    editUser: (id: string, newUserData: Partial<User>) => User,
+    getUser: (id: string) => User,
+    getAllOpenUsers: (idToExclude: string) => User[]
+};
