e8a91654e4165052661c954c59ee52f67546a191
ArielShabi <4ariel6@gmail.com>
2021-04-14 19:57:25 +0300

    update websocket client to have message subscription

--- whist-server/src/webSocketServer/webSocketClient.ts
+++ whist-server/src/webSocketServer/webSocketClient.ts
@@ -1,17 +1,55 @@
+import { tryParseJson } from '../utils';
+import { UserEventCallback } from './types';
+
 class WebSocketClient {
-    private connection: WebSocket;
+    #connection: WebSocket;
+    #events: { [key: string]: UserEventCallback[] }
 
     constructor(connection: WebSocket) {
-        this.connection = connection
+        this.#connection = connection;
+        this.#connection.onmessage = this.#onMessage;
     }
 
     sendMessage(type: string, data?: unknown) {
         const message = JSON.stringify({ type, data });
 
-        this.connection.send(message);
+        this.#connection.send(message);
     }
 
     get isReady() {
-        return this.connection.readyState === WebSocket.OPEN;
+        return this.#connection.readyState === WebSocket.OPEN;
+    }
+
+    on(event: string, callback: UserEventCallback): void {
+        if (!this.#events[event]) {
+            this.#events[event] = [callback];
+            return;
+        }
+
+        this.#events[event].push(callback);
+    }
+
+    off(eventType: string, callback: UserEventCallback): void {
+        if (!callback) {
+            this.#events[eventType] = [];
+        }
+
+        if (this.#events[eventType]) {
+            this.#events[eventType] = this.#events[eventType].filter(c => c !== callback)
+        }
+    }
+
+    #onMessage = (messageEvent: MessageEvent<string>): void => {
+        const { type, data } = messageEvent;
+
+        if (!this.#events[type] || this.#events[type].length === 0) {
+            return;
+        }
+
+        const message = tryParseJson(data);
+
+        this.#events[type].forEach(callback => {
+            callback(message);
+        });
     }
 }
