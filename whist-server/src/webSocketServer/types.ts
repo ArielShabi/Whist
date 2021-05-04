69edcf609fbd43d6aacdec525216f19a97345da5
ArielShabi <4ariel6@gmail.com>
2021-05-04 16:46:44 +0300

    added ? to interface

--- whist-server/src/webSocketServer/types.ts
+++ whist-server/src/webSocketServer/types.ts
@@ -1,7 +1,7 @@
 export type UserEventCallback = (message: unknown) => void
 
 export interface IWebSocketClient {
-    sendMessage(type: string, data: unknown): void;
+    sendMessage(type: string, data?: unknown): void;
     on(event: string, callback: UserEventCallback): void;
     off(eventType: string, callback: UserEventCallback): void;
     readonly isReady: boolean;
